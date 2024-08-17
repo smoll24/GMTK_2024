4dd8c89e0ed177a16937c322973016faf18a81d4
Sam Moll <115204665+smoll24@users.noreply.github.com>
2024-08-17 12:17:47 -0700

    Added text panel

---
+++ gmtk24/src/ui_viewport.gd
@@ -0,0 +1,33 @@
+extends Control
+
+var text_toggle
+
+@onready var text_open = $Text_open
+@onready var text_close = $Text_close
+@onready var officer_sprite = $Text_open/Officer_img
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#Close text panel
+	text_toggle = false
+	text_open.visible = false
+	text_close.visible = true
+	officer_sprite.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_close_text_pressed() -> void:
+	if text_toggle:
+		text_toggle = false
+		text_open.visible = false
+		text_close.visible = true
+
+func _on_open_text_pressed() -> void:
+	if not text_toggle:
+		text_toggle = true
+		text_open.visible = true
+		text_close.visible = false
