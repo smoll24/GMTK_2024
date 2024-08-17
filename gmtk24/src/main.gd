60a5dafc572ccb623d30641fa8b4f384ade65c74
Sam Moll <115204665+smoll24@users.noreply.github.com>
2024-08-16 19:27:01 -0700

    billion years

--- gmtk24/src/main.gd
+++ gmtk24/src/main.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if current_time - delta < 1.5768e+17:
+	if current_time - delta < 1.5768e+17: #billion years in seconds
 		current_time += delta
 	else:
 		current_time = 1.5768e+17
