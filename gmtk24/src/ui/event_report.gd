1f8669947bc59f313792e98a71f25d1f5254aa07
Christophe Moll <115204696+cmoll24@users.noreply.github.com>
2024-08-18 14:31:04 -0700

    Added event_report object

---
+++ gmtk24/src/ui/event_report.gd
@@ -0,0 +1,16 @@
+extends VBoxContainer
+class_name EventReport
+
+var event_dict
+
+func _init(event = null) -> void:
+	event_dict = event
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
