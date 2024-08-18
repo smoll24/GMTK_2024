e84000726893b20b9ceb7e4a4a863c1d3dd4ba11
Christophe Moll <115204696+cmoll24@users.noreply.github.com>
2024-08-18 12:00:51 -0700

    Fixed leap year issue

--- gmtk24/src/GlobalVariables.gd
+++ gmtk24/src/GlobalVariables.gd
@@ -3,6 +3,21 @@ extends Node
 var aspect_ratio = Vector2i(16,9)
 var viewport_size : Vector2
 
+var resource : Dictionary = {
+	'people' : {'amount' : 5000, 'growth' : 10},
+	'food' : {'amount' : 5000, 'growth' : 10},
+	'lumber' : {'amount' : 5000, 'growth' : 10},
+	'metal' : {'amount' : 5000, 'growth' : 10},
+	'energy' : {'amount' : 5000, 'growth' : 10},
+	'finance' : {'amount' : 5000, 'growth' : 10},
+	'comms' : {'amount' : 5000, 'growth' : 10},
+}
+
+const SEC_IN_MIN : int = 60
+const SEC_IN_HOUR : int = SEC_IN_MIN * 60
+const SEC_IN_DAY : int = SEC_IN_HOUR * 24
+const SEC_IN_YEAR : int = SEC_IN_DAY * 365.25
+
 func _ready():
 	viewport_size = get_viewport().get_visible_rect().size
 	#rescale_window()
@@ -11,13 +26,14 @@ func get_screen_size() -> Vector2:
 	return viewport_size
 
 func seconds_to_calendar(time_in_sec) -> Dictionary:
-	time_in_sec = int(time_in_sec)
-	var seconds = time_in_sec % 60
-	var minutes = (time_in_sec/60)%60
-	var hours = ((time_in_sec/60)/60)%24
-	var days = (((time_in_sec/60)/60)/24)%365
-	var years = (((time_in_sec/60)/60)/24)/365
-	days = days - years/4 + 1
+	var sec = time_in_sec
+	var years = int(sec/SEC_IN_YEAR)
+	var days = int((sec - years * SEC_IN_YEAR)/SEC_IN_DAY)
+	
+	sec = int(time_in_sec)
+	var hours = ((sec/60)/60)%24
+	var minutes = (sec/60)%60
+	var seconds = sec % 60
 	
 	return {'year' : years,
 			'day' : days,
@@ -35,9 +51,8 @@ func display_time_from_seconds(time_in_sec) -> String:
 	var years = calendar_time['year']
 	
 	var time : String = "%02d:%02d:%02d" % [hours, minutes, seconds]
-	
 	if days > 0:
-		time = "Day %02d " % [days] + time
+		time = "Day %02d " % [days + 2] + time
 	if years > 0:
 		time = "Year %s " % [format_number(years + 1970)] + time
 	return time
