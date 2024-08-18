47a060fe71289048cf0461d9c3009da7b6bab51c
Christophe Moll <115204696+cmoll24@users.noreply.github.com>
2024-08-17 19:39:22 -0700

    switch from engine.time_scale to sim_time_scale

--- gmtk24/src/main.gd
+++ gmtk24/src/main.gd
@@ -9,6 +9,8 @@ var current_time : float
 var time_skip_amount
 var fast_forward_until = 0
 
+var sim_time_scale : int = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current_time = 60*60*12
@@ -16,20 +18,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if current_time - delta < END_GAME_TIME:
-		current_time += delta
+	if current_time - delta * sim_time_scale < END_GAME_TIME:
+		current_time += delta * sim_time_scale
 	else:
 		current_time = END_GAME_TIME
 		print('YOU WIN')
-		Engine.time_scale = 1
+		sim_time_scale = 0
 	
 	background.update_time(current_time, delta)
 	current_time_label.text = "current time: " + GlobalVariables.display_time_from_seconds(current_time)
 	
 	if fast_forward_until < current_time:
-		Engine.time_scale = 1
+		sim_time_scale = 1
 
 func _on_fast_forward_pressed() -> void:
 	fast_forward_until = current_time + time_skip_amount
-	Engine.time_scale = time_skip_amount / 15
+	sim_time_scale = time_skip_amount / 15
 	time_skip_amount *= 2
