1f25400224e0a500d479b70d90bccb123563aad0
Sam Moll <115204665+smoll24@users.noreply.github.com>
2024-08-19 23:21:59 -0700

    Skill Tree Button

--- gmtk24/src/main.gd
+++ gmtk24/src/main.gd
@@ -166,3 +166,7 @@ func event_effect(event_type : Events.EVENT, outcome) -> void:
 func _on_restart_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://src/title_screen.tscn")
+
+
+func _on_skill_tree_toggle_pressed() -> void:
+	skill_tree.visible = not skill_tree.visible
